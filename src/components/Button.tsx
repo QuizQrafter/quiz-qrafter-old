2bc37e80423f3084d1f15bfbe9963eff1b701b99
Kelvin229 <ke20od@gmail.com>
2024-01-22 22:36:45 -0500

    prototype of the current figma design

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+type ButtonProps = {
+  href: string;
+  title: string;
+};
+
+const Button = ({ href, title }: ButtonProps) => {
+  return (
+    <Link href={href}>
+    <a href={href} className="inline-block bg-black text-white py-2 px-6 rounded hover:bg-gray-700 transition duration-300">{title}</a>
+    </Link>
+  );
+};
+
+export default Button;
